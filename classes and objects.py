aa7a5af0d3d6dcdb4a094e35e56f3571680879db
Earthtosarah <akinjisolasarah02@gmail.com>
2022-05-28 12:42:18 -0700

    CandO

---
+++ classes and objects.py
@@ -0,0 +1,31 @@
+class Student():
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, track, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.track = list(track)
+        self.score = float(score)  
+
+Peter= Student(change_name ="Peter", change_age=34, add_track=["UI/UX"], get_score=25.35)  
+
+def change_name(self, change_name):
+    self.change_name = change_name
+    print("New name is", change_name)
+    
+def change_age(self, change_age):
+    self.change_age = change_age
+    print("New age is", change_age)
+    
+def add_track(self, add_track):
+    self.add_track = add_track
+    print("New track is", add_track)
+def get_score(self, get_score):
+    self.add_track = get_score
+    print("New score is", get_score)
+    
+# Bob = Student(name="Bob", age=26, track=["FE","BE"],score=20.90)
+
+# Expected methods
+
+
+ 
